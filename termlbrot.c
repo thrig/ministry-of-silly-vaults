cb973fd8e961b0ac99d3736f0afc23b79fd1724a
Jeremy Mates <jmates@cpan.org>
2017-10-23 08:53:21 -0700

    Generation tied to terminal size. Also more random.

--- termlbrot.c
+++ termlbrot.c
@@ -1,45 +1,79 @@
 /* Mandelbrot, only at the resolution and graphics capabilities of a terminal */
 
+#include <sys/ioctl.h>
+#include <sys/ttycom.h>
+
 #include <err.h>
 #include <complex.h>
 #include <limits.h>
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 // TWEAK binary floor/wall offers too low a resolution, hence ~ for water
 // and , for rubble (colors might also help)
 #define FLOOR_TILES 8
-const char Floor_Tiles[FLOOR_TILES] =
-    { '~', '.', '.', '.', '.', ',', ',', '#' };
+const char *Floor_Tiles[FLOOR_TILES] =
+    { "\033[34m~", "\033[33m.", "\033[37m.", "\033[31m.", "\033[35m.", "\033[32m,", "\033[32m+", "\033[35m#" };
 
 unsigned int **make_matrixui(unsigned int rows, unsigned int cols);
 
 int main(void)
 {
-    // TWEAK area to iterate over
-    const long double run_real[2] = { -1.0, 1 };
-    const long double run_imag[2] = { -1.5, 0.5 };
     // TWEAK how big the terminal or output canvas is (and thus how much
     // resolution the fractal is displayed at)
-    const unsigned int rows = 39;
-    const unsigned int cols = 78;
-    // TODO can these autoscale depending on how tight the graph is?
-    // (lowering them will a) save CPU and b) increase open space)
-    const unsigned int iterations = 120;
-    const long double threshold = 2;
+    struct winsize w;
+    unsigned int rows = 23;
+    unsigned int cols = 80;
+
+    // TWEAK area to iterate over
+    long double run_real[2] = { -0.9, -0.7 };
+    long double run_imag[2] = { -1.0, 0.5 };
+
+    long double randval;
+
+    // TWEAK autoscale depending on how tight the graph is?
+    // lowering them will a) save CPU and b) increase open space
+    const unsigned int iterations = 100;
+    const long double threshold = 1.5;
 
     long double range_real, range_imag;
     long double cur_row, cur_col;
     long double complex coord, zeta;
 
-    unsigned int **field = make_matrixui(rows, cols);
+    unsigned int **field;
     long index;
     unsigned int stability;
     unsigned int min_stab = UINT_MAX;
     unsigned int max_stab = 0;
 
-    // TODO there's nothing that links the run ratio to the terminal ratio
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) {
+        warn("could not obtain terminal size from STDOUT");
+    } else {
+        cols = w.ws_col;
+        rows = w.ws_row - 2;
+    }
+
+    field = make_matrixui(rows, cols);
+
+    randval = 0.0 - arc4random() / (long double)UINT32_MAX;
+    printf("I0=%Lf", randval);
+    run_imag[0] = randval;
+
+    randval = 0.0 - arc4random() / (long double)UINT32_MAX;
+    printf(" R0=%Lf", randval);
+    run_real[0] = randval;
+
+    randval = arc4random() / (long double)UINT32_MAX;
+
+    // TWEAK divide factor is how much to zoom in by
+    run_real[1] = run_real[0] + randval / 4;
+    printf(" R1=%Lf\n", run_real[1]);
+
+    run_imag[1] = run_imag[0] + rows * (run_real[1]-run_real[0]) / (long double) cols * 3;
+
     range_real = run_real[1] - run_real[0];
     range_imag = run_imag[1] - run_imag[0];
 
@@ -82,10 +116,11 @@ int main(void)
                 index = 0;
             else if (index >= FLOOR_TILES - 1)
                 index = FLOOR_TILES - 1;
-            putchar(Floor_Tiles[index]);
+            printf("%s", Floor_Tiles[index]);
         }
         putchar('\n');
     }
+    printf("\033[0m");
 
     exit(EXIT_SUCCESS);
 }
