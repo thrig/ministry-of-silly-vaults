48a21ed157b6fb396abb9c872b899f294c2aabef
Jeremy Mates <jmates@cpan.org>
2019-07-05 07:15:28 -0700

    perl module deps. shuffle can be had from alexandria

--- util.lisp
+++ util.lisp
@@ -76,25 +76,6 @@
 (defmacro no-return (&body body)
   `(progn ,@body (values)))
 
-;;; nshuffle and friends borrowed from "Knuth shuffle" Rosetta Code page
-;;; TODO are these available anywhere in quicklisp?
-(defun nshuffle (sequence)
-  (etypecase sequence
-    (list  (nshuffle-list sequence))
-    (array (nshuffle-array sequence))))
-
-(defun nshuffle-list (list)
-  "Shuffle the list using an intermediate vector."
-  (let ((array (nshuffle-array (coerce list 'vector))))
-    (declare (dynamic-extent array))
-    (map-into list 'identity array)))
-
-(defun nshuffle-array (array)
-  (loop for i from (length array) downto 2
-        do (rotatef (aref array (random i))
-                    (aref array (1- i)))
-        finally (return array)))
-
 (defun poisson (l)
   (do ((L (exp (- l)))
        (p 1.0 (* p (random 1.0)))
