280b65d238dbebd046d8e62a41937031a286f993
Jeremy Mates <jeremy.mates@gmail.com>
2019-09-02 15:20:32 +0000

    rogue odds

--- pseudo-random-dist/Odds.pm
+++ pseudo-random-dist/Odds.pm
@@ -16,14 +16,18 @@ use namespace::clean;
 
 has odds   => ( is => 'rw' );
 has float  => ( is => 'lazy' );
+# from rogue 3.6.3 the very bad
+#define RN (((seed = seed*11109+13849) & 0x7fff) >> 1)
+has rogue  => ( is => 'lazy' );
 has uint32 => ( is => 'lazy' );
 has uint64 => ( is => 'lazy' );
 
 sub BUILD { die "need odds" unless exists $_[1]->{odds} }
 sub _build_float  { $_[0]->odds / 100 }
-sub _build_uint32 { int( ( $_[0]->odds / 100 ) * 4294967296 ) }
+sub _build_rogue  { int( ( $_[0]->odds / 100 ) * 16383 ) }
+sub _build_uint32 { int( ( $_[0]->odds / 100 ) * 4294967295 ) }
 sub _build_uint64 { int( ( $_[0]->odds / 100 ) * 18446744073709551615 ) }
 
-sub all { join $/, map $_[0]->$_, qw(odds float uint32 uint64) }
+sub all { join $/, map $_[0]->$_, qw(odds float rogue uint32 uint64) }
 
 1;
